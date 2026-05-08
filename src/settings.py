964dfde6569c1cfe1d827a3ff69867ecbcff9d7e
subaru <islomibadullaev020@gmail.com>
2026-05-08 13:48:18 +0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
 
     'my_app.apps.MyAppConfig',
 
