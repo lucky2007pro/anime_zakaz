e2dc1b47c9ec602eec3f305defd8dbdedd93a9e2
subaru <islomibadullaev020@gmail.com>
2026-07-26 15:58:35 +0500

    Update context_processors.py

--- my_app/context_processors.py
+++ my_app/context_processors.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from .models import Notice
+from .models import Notice, ProfileAvatar, WatchHistory
 
 
 def notices_context(request):
@@ -11,3 +11,29 @@ def notices_context(request):
     else:
         count = 0
     return {'unread_notices_count': count}
+
+
+def navbar_extra(request):
+    """
+    base.html (mobil navbar) uchun kerak bo'ladigan qo'shimcha ma'lumotlar:
+    - avatars: admin yuklagan barcha profil rasmlari (avatar picker uchun)
+    - last_watched_movies: foydalanuvchining oxirgi ko'rgan 2 ta animesi
+    """
+    if not request.user.is_authenticated:
+        return {}
+
+    avatars = ProfileAvatar.objects.all().order_by('-created_at')
+
+    last_watched_movies = [
+        w.movie for w in (
+            WatchHistory.objects
+            .filter(user=request.user)
+            .select_related('movie')
+            .order_by('-last_watched')[:2]
+        )
+    ]
+
+    return {
+        'avatars': avatars,
+        'last_watched_movies': last_watched_movies,
+    }
